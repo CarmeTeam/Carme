01bfb1228027dc9509c647a4ec263a162297399a
Philipp Reusch <reusch@itwm.fhg.de>
2019-10-02 15:36:06 +0200

    Added epilog script to notify about job termination.

---
+++ Carme-Scripts/slurm/slurm-epilog-scripts/epilog.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#
+# TODO add some documentary
+#
+
+if [[ -z "$LAUNCH_ADDR" ] || [ "$(hostname -i)" == "$LAUNCH_ADDR"]]; then
+    echo "I am the launch node and will report that the job finished."
+    python notify_job_finished.py SLURM_JOBID CARME_BACKEND_SERVER CARME_BACKEND_PORT
+fi
