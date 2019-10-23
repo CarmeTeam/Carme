45fdbfd1fb327f2d6ba932cdc568c78ad6e30803
carme-admin <carme-admin@carme.ai>
2019-10-23 12:01:55 +0200

    Use get_variable function to add only the needed variables from the config file and clean code.

--- Carme-Scripts/frontend/slurm-worker.sh
+++ Carme-Scripts/frontend/slurm-worker.sh
@@ -6,13 +6,10 @@
 # @ Dr. Dominik Straßel, Janis Keuper 2018
 #-----------------------------------------------------------------------------------------------------------------------------------
 
-
-LOGDIR="/home/$USER/.job-log-dir"
-if [ ! -d $LOGDIR ]; then
-    mkdir $LOGDIR
-fi
 #-----------------------------------------------------------------------------------------------------------------------------------
-cd /home/$USER/
+cd ${HOME}
+#-----------------------------------------------------------------------------------------------------------------------------------
+
 DBJOBID=$1
 IMAGE=$2
 mountstr=$3  
@@ -21,12 +18,31 @@ MEM=$5
 CARME_SCRIPT_PATH=$6
 GPU_TYPE=$7
 
-#read user accessable part of CarmeConfig                              
-source ${CARME_SCRIPT_PATH}/../InsideContainer/CarmeConfig.container   
+#-----------------------------------------------------------------------------------------------------------------------------------
+# needed variables from ${CARME_SCRIPT_PATH}/../InsideContainer/CarmeConfig.container
+CONFIG_FILE="${CARME_SCRIPT_PATH}/../InsideContainer/CarmeConfig.container"
+if [ -f ${CONFIG_FILE} ];then
+  function get_variable () {
+    variable_value=$(grep --color=never -Po "^${1}=\K.*" "${2}")
+    variable_value=${variable_value%#*}
+    variable_value=${variable_value%#*}
+    variable_value=$(echo "$variable_value" | tr -d '"')
+    echo $variable_value
+  }
+else
+  echo "${CONFIG_FILE} not found!"
+  exit 137
+fi
+
+CARME_GATEWAY=$(get_variable CARME_GATEWAY ${CONFIG_FILE})
+CARME_BACKEND_SERVER=$(get_variable CARME_BACKEND_SERVER ${CONFIG_FILE})
+CARME_BACKEND_PORT=$(get_variable CARME_BACKEND_PORT ${CONFIG_FILE})
+#-----------------------------------------------------------------------------------------------------------------------------------
+
 MOUNTS=${mountstr//[_]/ }
 
 IPADDR=$(ip route get ${CARME_GATEWAY} | head -1 | awk '{print $5}' | cut -d/ -f1)
-if [[ -z $IPADDR ]];then
+if [[ -z ${IPADDR} ]];then
   echo "ERROR: IP not set!"
 		exit 137
 fi
@@ -42,14 +58,14 @@ echo "                 - Image: ${IMAGE}"
 echo ""
 
 GPU_DEVICES=${CUDA_VISIBLE_DEVICES}
-if [[ -z "$GPU_DEVICES" ]];then
+if [[ -z "${GPU_DEVICES}" ]];then
   echo "ERROR: WORKER: available GPUs not set!"
   echo "ERROR: WORKER: no free GPUs on node. Job stops now!"
   echo "ERROR: please contact your admin."
   exit 137
 fi
 
-echo "WORKER GPUS: " $IPADDR $GPUS $GPU_DEVICES
+echo "WORKER GPUS: " ${IPADDR} ${GPUS} ${GPU_DEVICES}
 if [[ "${GPU_TYPE}" == "default" ]];then
   echo "WORKER GPUS: #(GPUS): ${GPUS}, GPU-Devices: ${GPU_DEVICES}, GPU type not specified"
 else
@@ -59,8 +75,8 @@ echo ""
 
 #crate SSD scratch folder------------------------------------
 if [[ -d /scratch_local ]];then
-  mkdir -p /scratch_local/$SLURM_JOBID
-  echo "/home/SSD is a fast local scratch storage. WARMING: everything will be deleted at the end of this job!" > /scratch_local/$SLURM_JOBID/readme.md
+  mkdir -p /scratch_local/${SLURM_JOB_ID}
+  echo "/home/SSD is a fast local scratch storage. WARMING: everything will be deleted at the end of this job!" > /scratch_local/${SLURM_JOB_ID}/readme.md
 else
 	 echo "ERROR: WORKER: cannot create folder on local SSD"
 		echo "               SSD parent folder not found"
@@ -68,6 +84,6 @@ fi
 #-------------------------------------------------------------                        
 
 #start singularity ------------------------------------
-newpid singularity exec -B /etc/libibverbs.d $MOUNTS -B /scratch_local/$SLURM_JOBID:/home/SSD $IMAGE /bin/bash /home/.CarmeScripts/start_worker.sh $IPADDR $NB_PORT $TB_PORT $USER $HASH $GPU_DEVICES $MEM 
+newpid singularity exec -B /etc/libibverbs.d ${MOUNTS} -B /scratch_local/${SLURM_JOB_ID}:/home/SSD ${IMAGE} /bin/bash /home/.CarmeScripts/start_worker.sh ${IPADDR} ${NB_PORT} ${TB_PORT} ${USER} ${HASH} ${GPU_DEVICES} ${MEM}
 #------------------------------------------------------
 
