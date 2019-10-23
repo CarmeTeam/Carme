8b7b5fcc9d21f4e171a525b6fa6f166457a5217b
carme-admin <carme-admin@carme.ai>
2019-10-23 11:42:18 +0200

    Use get_variable function to add only the needed variables from the config file.

--- Carme-Scripts/frontend/delete_route.sh
+++ Carme-Scripts/frontend/delete_route.sh
@@ -1,20 +1,28 @@
 #!/bin/bash
-# ----------------------------------------------
-# Carme                                         
-# ----------------------------------------------
-# delete_route.sh - deletes dummy proxy                                                                                                                                                                    
+# ----------------------------------------------------------------------------------------------------------------------------------
+# Carme
+# ----------------------------------------------------------------------------------------------------------------------------------
+# delete_route.sh - deletes dummy proxy
 # 
 # usage: delete_route
-#                                               
+# 
 # see Carme development guide for documentation:
 # * Carme/Carme-Doc/DevelDoc/CarmeDevelopmentDocu.md
-#                                                   
-# Copyright 2019 by Fraunhofer ITWM                 
-# License: http://open-carme.org/LICENSE.md         
-# Contact: info@open-carme.org                      
-# ---------------------------------------------   
+#
+# Copyright 2019 by Fraunhofer ITWM
+# License: http://open-carme.org/LICENSE.md
+# Contact: info@open-carme.org
+# ----------------------------------------------------------------------------------------------------------------------------------
 
-source ../../CarmeConfig
+function get_variable () {
+  variable_value=$(grep --color=never -Po "^${1}=\K.*" "${2}")
+  variable_value=${variable_value%#*}
+  variable_value=$(echo "$variable_value" | tr -d '"')
+  echo $variable_value
+}
+CARME_PROXY_PATH=$(get_variable CARME_PROXY_PATH $CLUSTER_DIR/${CONFIG_FILE})
+
+#-----------------------------------------------------------------------------------------------------------------------------------
 
 touch ${CARME_PROXY_PATH}/routes/dynamic/test
 sleep 10
