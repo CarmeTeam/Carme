bc5cad2afa8a07ed78e6d6a6797c19b1a33b6db5
carme-admin <carme-admin@carme.ai>
2019-11-08 15:39:27 +0100

    make anaconda environment visible for users

--- Carme-Scripts/InsideContainer/base_bashrc.sh
+++ Carme-Scripts/InsideContainer/base_bashrc.sh
@@ -91,6 +91,19 @@ fi
 #-----------------------------------------------------------------------------------------------------------------------------------
 
 
+# make anaconda environment visible for users --------------------------------------------------------------------------------------
+__conda_setup="$('/opt/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+  eval "$__conda_setup"
+else
+  if [ -f "/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+    . "/opt/anaconda3/etc/profile.d/conda.sh"
+  fi
+fi
+unset __conda_setup
+#-----------------------------------------------------------------------------------------------------------------------------------
+
+
 # define cancel function -----------------------------------------------------------------------------------------------------------
 function carme_canceljob() {
     NUMBERCHECK='^[0-9]+$'
