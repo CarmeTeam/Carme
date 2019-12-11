7de3a72527117aabcc17a31a532432968282408b
Philipp Reusch <reusch@itwm.fhg.de>
2019-12-11 10:19:50 +0100

    Load CARME_* from carme config file.

--- Carme-Scripts/slurm/slurmctld-epilog-scripts/epilog.sh
+++ Carme-Scripts/slurm/slurmctld-epilog-scripts/epilog.sh
@@ -4,4 +4,22 @@
 # notify carme about the job epilog
 #
 
+# define function to get variables from CarmeConfig --------------------------------------------------------------------------------
+function get_variable () {
+  variable_value=$(grep --color=never -Po "^${1}=\K.*" "${2}")
+  variable_value=${variable_value%#*}
+  variable_value=${variable_value%#*}
+  variable_value=$(echo "$variable_value" | tr -d '"')
+  echo $variable_value
+}
+
+# source needed variables
+CONFIG_FILE="/opt/Carme/CarmeConfig"
+CARME_BACKEND_SERVER=$(get_variable CARME_BACKEND_SERVER ${CONFIG_FILE})
+CARME_BACKEND_PORT=$(get_variable CARME_BACKEND_PORT ${CONFIG_FILE})
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+
+# call notify_job_epilog -----------------------------------------------------------------------------------------------------------
 python notify_job_epilog.py SLURM_JOB_ID SLURM_JOB_USER CARME_BACKEND_SERVER CARME_BACKEND_PORT
+# ----------------------------------------------------------------------------------------------------------------------------------
--- Carme-Scripts/slurm/slurmctld-prolog-scripts/prolog.sh
+++ Carme-Scripts/slurm/slurmctld-prolog-scripts/prolog.sh
@@ -4,4 +4,22 @@
 # notify carme about the job prolog
 #
 
+# define function to get variables from CarmeConfig --------------------------------------------------------------------------------
+function get_variable () {
+  variable_value=$(grep --color=never -Po "^${1}=\K.*" "${2}")
+  variable_value=${variable_value%#*}
+  variable_value=${variable_value%#*}
+  variable_value=$(echo "$variable_value" | tr -d '"')
+  echo $variable_value
+}
+
+# source needed variables
+CONFIG_FILE="/opt/Carme/CarmeConfig"
+CARME_BACKEND_SERVER=$(get_variable CARME_BACKEND_SERVER ${CONFIG_FILE})
+CARME_BACKEND_PORT=$(get_variable CARME_BACKEND_PORT ${CONFIG_FILE})
+# ----------------------------------------------------------------------------------------------------------------------------------
+
+
+# call notify_job_prolog -----------------------------------------------------------------------------------------------------------
 python notify_job_prolog.py SLURM_JOB_ID SLURM_JOB_USER CARME_BACKEND_SERVER CARME_BACKEND_PORT
+# ----------------------------------------------------------------------------------------------------------------------------------
