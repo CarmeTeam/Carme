37430a73b97d69ddcf21ca9d4e26e57067f8dafe
Dominik S <dominik.strassel@itwm.fraunhofer.de>
2020-02-20 15:21:34 +0100

    change the way to init conda so that it also works with newer conda versions

--- Carme-Scripts/InsideContainer/base_bashrc.sh
+++ Carme-Scripts/InsideContainer/base_bashrc.sh
@@ -58,19 +58,9 @@ fi
 
 
 # make anaconda environment visible ------------------------------------------------------------------------------------------------
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/opt/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/opt/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/opt/anaconda3/etc/profile.d/conda.sh"
-    else
-        export PATH="/opt/anaconda3/bin:$PATH"
-    fi
+if [[ -f "/opt/anaconda3/etc/profile.d/conda.sh" ]];then
+  source "/opt/anaconda3/etc/profile.d/conda.sh"
+  conda activate base
 fi
-unset __conda_setup
-# <<< conda initialize <<<
 #-----------------------------------------------------------------------------------------------------------------------------------
 
