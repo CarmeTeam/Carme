0ce68bd6575eca8187a2ca525d24b91afaa31300
Dominik S <dominik.strassel@itwm.fraunhofer.de>
2020-03-30 16:52:55 +0200

    conda and ssh

--- Carme-Scripts/InsideContainer/base_bashrc.sh
+++ Carme-Scripts/InsideContainer/base_bashrc.sh
@@ -4,32 +4,8 @@
 #
 
 
-# CARME specific changes -----------------------------------------------------------------------------------------------------------
-set -e
-set -o pipefail
-
-CARME_SCRIPTS_DIR="/home/.CarmeScripts"
-
-
-# add specific bash functions
-CARME_BASH_FUNCTIONS="${CARME_SCRIPTS_DIR}/carme_bash_functions.sh"
-[[ -f "${CARME_BASH_FUNCTIONS}" ]] && source "${CARME_BASH_FUNCTIONS}"
-
-
-# add job specific bash settings
-CARME_JOB_DIR="${HOME}/.local/share/carme/job/${SLURM_JOB_ID}"
-[[ -f "${CARME_JOB_DIR}/bashrc" ]] && source "${CARME_JOB_DIR}/bashrc"
-
-
-# add variables that should be availabe in ssh
-[[ -f "${CARME_JOB_DIR}/ssh/envs/$(hostname)" ]] && source "${CARME_JOB_DIR}/ssh/envs/$(hostname)"
-
-set +e
-set +o pipefail
-#-----------------------------------------------------------------------------------------------------------------------------------
-
-
 if [[ "$-" = *i* ]];then
+
   # redefine prompt and its color (can be overwritten in .bash_aliases) ------------------------------------------------------------
   export PS1="[\[\033[01;35m\]\u\[\033[m\]@\[\033[01;32m\]\h\[\033[m\]:\[\033[01;31;1m\]\W\[\033[m\]]\$ "
   #---------------------------------------------------------------------------------------------------------------------------------
@@ -39,10 +15,8 @@ if [[ "$-" = *i* ]];then
   # don't add duplicate lines or lines starting with space
   HISTCONTROL=ignoreboth
 
-
   # append to history
   shopt -s histappend
-
 
   # set history length
   HISTSIZE=1000
@@ -59,9 +33,36 @@ if [[ "$-" = *i* ]];then
   # --------------------------------------------------------------------------------------------------------------------------------
 
 
-# make anaconda environment visible ------------------------------------------------------------------------------------------------
-if [[ -f "/opt/anaconda3/etc/profile.d/conda.sh" ]];then
-  source "/opt/anaconda3/etc/profile.d/conda.sh"
+  # add user specific bash settings ------------------------------------------------------------------------------------------------
+  [[ -f "${HOME}/.bash_aliases" ]] && source "${HOME}/.bash_aliases"
+  #---------------------------------------------------------------------------------------------------------------------------------
+
+
+  # CARME specific changes ---------------------------------------------------------------------------------------------------------
+  CARME_SCRIPTS_DIR="/home/.CarmeScripts"
+
+  # add specific bash functions
+  CARME_BASH_FUNCTIONS="${CARME_SCRIPTS_DIR}/carme_bash_functions.sh"
+  [[ -f "${CARME_BASH_FUNCTIONS}" ]] && source "${CARME_BASH_FUNCTIONS}"
+
+  # add job specific bash settings
+  CARME_JOB_BASH="${HOME}/.local/share/carme/tmp-files-${SLURM_JOB_ID}/bash_${SLURM_JOB_ID}"
+  [[ -f "${CARME_JOB_BASH}" ]] && source "${CARME_JOB_BASH}"
+
+  # add terminal welcome message
+  CARME_MESSAGES="${CARME_SCRIPTS_DIR}/carme-messages.sh"
+  [[ -f "${CARME_MESSAGES}" ]] && source "${CARME_MESSAGES}"
+  #---------------------------------------------------------------------------------------------------------------------------------
+else
+  export PS1="[\u@\h:\W]\$ "
+fi
+
+
+# activate conda base environment --------------------------------------------------------------------------------------------------
+# NOTE: conda should always be activated not only in interactive shells
+CONDA_INIT_FILE="/opt/anaconda3/etc/profile.d/conda.sh"
+if [[ -f "${CONDA_INIT_FILE}" ]];then
+  source "${CONDA_INIT_FILE}"
   conda activate base
 fi
 #-----------------------------------------------------------------------------------------------------------------------------------
