754c553f59a6f9e86132edb46d38f08b8520994d
Dominik S <dominik.strassel@itwm.fraunhofer.de>
2020-07-10 10:22:46 +0200

    add carme env variables for job-id, -name, and -gpus

--- Carme-Scripts/slurm/slurm-prolog-scripts/carme-node-prolog.sh
+++ Carme-Scripts/slurm/slurm-prolog-scripts/carme-node-prolog.sh
@@ -174,6 +174,9 @@ export CARME_NODES=$(scontrol show hostname "${NODE_LIST}" | paste -d, -s)
 export CARME_MASTER=${MASTER_NODE}
 export CARME_START_SSHD=${CARME_START_SSHD}
 export CARME_LOCAL_SSD_PATH=${CARME_LOCAL_SSD_PATH}
+export CARME_JOB_ID=${SLURM_JOB_ID}
+export CARME_JOB_NAME=${SLURM_JOB_NAME}
+export CARME_JOB_GPUS=${SLURM_JOB_GPUS}
 " > "${JOBDIR}/bashrc"
 
 
