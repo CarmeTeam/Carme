aaeabcb045ae4d7345792039495a7b78a0c13cee
Dominik S <dominik.strassel@itwm.fraunhofer.de>
2020-07-10 13:38:58 +0200

    checking here for non existing bash variables results in crashing terminals and non functional jobs and does not help at all

--- Carme-Scripts/InsideContainer/Carme-Messages/00-carme-general-info.sh
+++ Carme-Scripts/InsideContainer/Carme-Messages/00-carme-general-info.sh
@@ -17,12 +17,12 @@ echo ""
 
 # print job information
 echo -e "\033[1mJob Information ------------------------------\033[0m"
-echo "Job-ID|-Name: ${CARME_JOB_ID:?"not set"} | ${CARME_JOB_NAME:?"not set"}"
-echo "Nodes:        ${CARME_NODES:?"not set"}"
-if [[ -n "$(echo "${CARME_JOB_GPUS:?"not set"}" | tr ',' '\n' | wc -l)" ]];then
-  echo "GPUs/Node:    $(echo "${CARME_JOB_GPUS:?"not set"}" | tr ',' '\n' | wc -l)"
+echo "Job-ID|-Name: ${CARME_JOB_ID} | ${CARME_JOB_NAME}"
+echo "Nodes:        ${CARME_NODES}"
+if [[ -n "$(echo "${CARME_JOB_GPUS}" | tr ',' '\n' | wc -l)" ]];then
+  echo "GPUs/Node:    $(echo "${CARME_JOB_GPUS}" | tr ',' '\n' | wc -l)"
 fi
-if [[ -n "${CARME_JOB_GPUS:?"not set"}" ]];then
-  echo "GPU-ID(s):    ${CARME_JOB_GPUS:?"not set"}"
+if [[ -n "${CARME_JOB_GPUS}" ]];then
+  echo "GPU-ID(s):    ${CARME_JOB_GPUS}"
 fi
-echo "End-Time:   $(grep "^${CARME_JOB_ID:?"not set"}[[:space:]]${CARME_JOB_NAME:?"not set"}" .local/share/carme/job-log-dir/job-log.dat | awk '{print $6}')"
+echo "End-Time:   $(grep "^${CARME_JOB_ID}[[:space:]]${CARME_JOB_NAME}" .local/share/carme/job-log-dir/job-log.dat | awk '{print $6}')"
