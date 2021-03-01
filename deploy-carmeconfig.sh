0a035c771c2be4b5d31732864803ffaa56c3069e
Dominik S <dominik.strassel@itwm.fraunhofer.de>
2021-03-01 16:44:30 +0100

    update deploy carme config for use over different nodes

--- deploy-carmeconfig.sh
+++ deploy-carmeconfig.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 #-----------------------------------------------------------------------------------------------------------------------------------
 # deployCarmeConfig.sh deploys the CarmeConfig into separate files
-# - CarmeConfig.frontend
-# - CarmeConfig.backend
-# - CarmeConfig.container
-#-----------------------------------------------------------------------------------------------------------------------------------
-# USAGE: In order to run this script you have to be `root` and run it as `bash deployCarmeConfig.sh`.
-#-----------------------------------------------------------------------------------------------------------------------------------
-# COPYRIGHT: Fraunhofer ITWM, 2019
+#
+# COPYRIGHT: Fraunhofer ITWM, 2021
 # LICENCE: http://open-carme.org/LICENSE.md 
 # CONTACT: info@open-carme.org
 #----------------------------------------------------------------------------------------------------------------------------------
@@ -47,7 +42,7 @@ is_root
 
 
 # define function to extract parameters from another file --------------------------------------------------------------------------
-VARIABLES_PARAMETER_FILE="/opt/Carme/variables.conf"
+VARIABLES_PARAMETER_FILE="variables.conf"
 if [[ -f ${VARIABLES_PARAMETER_FILE} ]];then
   function get_parameter () {
     PARAMETER=$(grep --color=never -Po "^${1}=\K.*" "${VARIABLES_PARAMETER_FILE}")
@@ -60,15 +55,6 @@ fi
 #-----------------------------------------------------------------------------------------------------------------------------------
 
 
-# source needed variables /opt/Carme/CarmeConfig -----------------------------------------------------------------------------------
-CARME_FRONTEND_PATH=$(get_variable CARME_FRONTEND_PATH)
-[[ -z ${CARME_FRONTEND_PATH} ]] && die "CARME_FRONTEND_PATH not set"
-
-CARME_SCRIPTS_PATH=$(get_variable CARME_SCRIPTS_PATH)
-[[ -z ${CARME_SCRIPTS_PATH} ]] && die "CARME_SCRIPTS_PATH not set"
-#-----------------------------------------------------------------------------------------------------------------------------------
-
-
 # source the variables that have to be imported from /opt/Carme/CarmeConfig --------------------------------------------------------
 FRONTEND_VARIABLES=$(get_parameter FRONTEND_VARIABLES)
 CONTAINER_VARIABLES=$(get_parameter CONTAINER_VARIABLES)
@@ -76,23 +62,27 @@ BACKEND_VARIABLES=$(get_parameter BACKEND_VARIABLES)
 #-----------------------------------------------------------------------------------------------------------------------------------
 
 
-# get date -------------------------------------------------------------------------------------------------------------------------
-TODAY=$(date "+%d-%m-%Y")
+# source needed variables /opt/Carme/CarmeConfig -----------------------------------------------------------------------------------
+CARME_FRONTEND_PATH=$(get_variable CARME_FRONTEND_PATH)
+CARME_SCRIPTS_PATH=$(get_variable CARME_SCRIPTS_PATH)
+CARME_LOGINNODE_NAME=$(get_variable CARME_LOGINNODE_NAME)
+CARME_NODES_LIST=$(get_variable CARME_NODES_LIST)
+
+[[ -z ${CARME_FRONTEND_PATH} ]] && die "CARME_FRONTEND_PATH not set"
+[[ -z ${CARME_SCRIPTS_PATH} ]] && die "CARME_SCRIPTS_PATH not set"
+[[ -z ${CARME_LOGINNODE_NAME} ]] && die "CARME_LOGINNODE_NAME not set"
+[[ -z ${CARME_NODES_LIST} ]] && die "CARME_NODES_LIST not set"
 #-----------------------------------------------------------------------------------------------------------------------------------
 
 
-# define path where carme in installed ---------------------------------------------------------------------------------------------
-CARME_BASE_PATH="/opt/Carme"
+# define carme config file ---------------------------------------------------------------------------------------------------------
+CONFIG_FILE="CarmeConfig"
 #-----------------------------------------------------------------------------------------------------------------------------------
 
 
 # collect variables needed in the frontend -----------------------------------------------------------------------------------------
-echo "collect variables needed in the webfrontend"
+echo "CarmeConfig.frontend: collect variables needed in the webfrontend"
 
-if [[ -f "${CARME_FRONTEND_PATH}/CarmeConfig.frontend" ]];then
-  mv "${CARME_FRONTEND_PATH}/CarmeConfig.frontend" "${CARME_FRONTEND_PATH}/CarmeConfig.frontend_old" || die "cannot backup CarmeConfig.frontend"
-  echo "saved previous CarmeConfig.frontend as CarmeConfig.frontend_old"
-fi
 
 echo "
 #-----------------------------------------------------------------------------------------------------------------------------------
@@ -100,41 +90,35 @@ echo "
 #
 # WARNING: This file is generated automatically by deployCarmeConfig.sh. Do not edit manually!
 #-----------------------------------------------------------------------------------------------------------------------------------
-" > CarmeConfig.frontend
+" > "CarmeConfig.frontend.new"
+
 
 for VARIABLE in ${FRONTEND_VARIABLES};do
-  grep "^${VARIABLE}=" "${CONFIG_FILE}" >> CarmeConfig.frontend
+  grep "^${VARIABLE}=" "${CONFIG_FILE}" >> CarmeConfig.frontend.new
 done
 
 
 # change permissions of new CarmeConfig.frontend
-chmod 600 CarmeConfig.frontend || die "cannot change file permissions of CarmeConfig.frontend"
-echo "changed permission of CarmeConfig.frontend to 600"
-
-
-# move new CarmeConfig.frontend CARME_FRONTEND_PATH
-mv CarmeConfig.frontend "${CARME_FRONTEND_PATH}/CarmeConfig.frontend" || die "cannot move CarmeConfig.frontend to ${CARME_FRONTEND_PATH}"
-echo "moved CarmeConfig.frontend to ${CARME_FRONTEND_PATH}"
+chmod 600 "CarmeConfig.frontend.new" || die "cannot change file permissions of CarmeConfig.frontend.new"
 
 
 # change ownership of CarmeConfig.frontend
-chown www-data:www-data "${CARME_FRONTEND_PATH}/CarmeConfig.frontend"
+chown www-data:www-data "CarmeConfig.frontend.new"
 
 
-# check modification date
-MODIFYDATE_FRONTEND_CONFIG=$(date -r "${CARME_FRONTEND_PATH}"/CarmeConfig.frontend "+%d-%m-%Y")
-[[ "${MODIFYDATE_FRONTEND_CONFIG}" != "${TODAY}" ]] && die "CarmeConfig.frontend not modified (last modification ${MODIFYDATE_FRONTEND_CONFIG})"
+# move carme config frontend to right folder
+ssh ${COMPUTE_NODE} -X -t "mv ${CARME_FRONTEND_PATH}/CarmeConfig.frontend ${CARME_FRONTEND_PATH}/CarmeConfig.frontend.bak"
+scp -p "CarmeConfig.frontend.new" "${CARME_LOGINNODE_NAME}:${CARME_FRONTEND_PATH}/CarmeConfig.frontend"
+
+
+# remove helper config
+rm "CarmeConfig.frontend.new"
 #-----------------------------------------------------------------------------------------------------------------------------------
 
 
 # collect variables needed inside the containers -----------------------------------------------------------------------------------
-echo ""
-echo "collect variables needed inside the containers"
+echo "CarmeConfig.container: collect variables needed in the job"
 
-if [[ -f "${CARME_SCRIPTS_PATH}/InsideContainer/CarmeConfig.container" ]];then
-  mv "${CARME_SCRIPTS_PATH}/InsideContainer/CarmeConfig.container" "${CARME_SCRIPTS_PATH}/InsideContainer/CarmeConfig.container_old"  || die "cannot backup CarmeConfig.container"
-  echo "saved previous CarmeConfig.container as CarmeConfig.container_old"
-fi
 
 echo "
 #-----------------------------------------------------------------------------------------------------------------------------------
@@ -142,38 +126,38 @@ echo "
 # 
 # WARNING: This file is generated automatically by deployCarmeConfig.sh. Do not edit manually!
 #-----------------------------------------------------------------------------------------------------------------------------------
-" > CarmeConfig.container
+" > CarmeConfig.container.new
+
 
 for VARIABLE in ${CONTAINER_VARIABLES};do
-  grep "^${VARIABLE}=" "${CONFIG_FILE}" >> CarmeConfig.container
+  grep "^${VARIABLE}=" "${CONFIG_FILE}" >> CarmeConfig.container.new
 done
 
 
 # change permissions of new CarmeConfig.container
-chmod 644 CarmeConfig.container || die "cannot change file permissions of CarmeConfig.container"
-echo "changed permission of CarmeConfig.container to 644"
+chmod 644 CarmeConfig.container.new || die "cannot change file permissions of CarmeConfig.container.new"
 
 
-# move new CarmeConfig.container to CARME_SCRIPTS_PATH/InsideContainer
-mv CarmeConfig.container "${CARME_SCRIPTS_PATH}/InsideContainer/CarmeConfig.container" || die "cannot move CarmeConfig.container ${CARME_SCRIPTS_PATH}/InsideContainer"
-echo "moved CarmeConfig.container to ${CARME_SCRIPTS_PATH}/InsideContainer"
+# move carme config frontend to right folder
+mv "CarmeConfig.container.new" "computenode/Carme-Scripts/InsideContainer/CarmeConfig.container"
 
 
-# check modification date
-MODIFYDATE_CONTAINER_CONFIG=$(date -r "${CARME_SCRIPTS_PATH}/InsideContainer/CarmeConfig.container" "+%d-%m-%Y")
-[[ "${MODIFYDATE_CONTAINER_CONFIG}" != "${TODAY}" ]] && die "${CARME_SCRIPTS_PATH}/InsideContainer/CarmeConfig.container not modified (last modification ${MODIFYDATE_FRONTEND_CONFIG})"
+for COMPUTE_NODE in ${CARME_NODES_LIST}; do
+  echo -e "${COMPUTE_NODE}:\tcopy computenode files to ${COMPUTE_NODE}:${CARME_INSTALL_DIR}"
+  ssh ${COMPUTE_NODE} -X -t "mv ${CARME_SCRIPTS_PATH}/InsideContainer/CarmeConfig.container ${CARME_SCRIPTS_PATH}/InsideContainer/CarmeConfig.container.bak"
+  scp -p "CarmeConfig.container.new" "${COMPUTE_NODE}:${CARME_SCRIPTS_PATH}/InsideContainer/CarmeConfig.container"
+  echo ""
+done
+
+
+# remove helper config
+rm "CarmeConfig.container.new"
 #-----------------------------------------------------------------------------------------------------------------------------------
 
 
 # collect variables needed in the backend ------------------------------------------------------------------------------------------
-echo ""
-echo "collect variables needed in the backend"
+echo "CarmeConfig.backend: collect variables needed in the backend"
 
-
-if [[ -f "${CARME_BASE_PATH}/CarmeConfig.backend" ]];then
-  mv "${CARME_BASE_PATH}"/CarmeConfig.backend "${CARME_BASE_PATH}"/CarmeConfig.backend_old  || die "cannot backup CarmeConfig.backend"
-  echo "saved previous CarmeConfig.backend as CarmeConfig.backend_old"
-fi
 
 echo "
 #-----------------------------------------------------------------------------------------------------------------------------------
@@ -181,17 +165,18 @@ echo "
 #
 # WARNING: This file is generated automatically by deployCarmeConfig.sh. Do not edit manually!
 #-----------------------------------------------------------------------------------------------------------------------------------
-" > "${CARME_BASE_PATH}"/CarmeConfig.backend
+" > "CarmeConfig.backend.new"
+
 
 for VARIABLE in ${BACKEND_VARIABLES};do
-  grep "^${VARIABLE}=" "${CONFIG_FILE}" >> "${CARME_BASE_PATH}"/CarmeConfig.backend
+  grep "^${VARIABLE}=" "${CONFIG_FILE}" >> "CarmeConfig.backend.new"
 done
 
 
 # change permissions of new CarmeConfig.backend
-chmod 644 "${CARME_BASE_PATH}"/CarmeConfig.backend || die "cannot change file permissions of CarmeConfig.backend"
-echo "changed permission of CarmeConfig.backend to 644"
+chmod 644 "CarmeConfig.backend.new" || die "cannot change file permissions of CarmeConfig.backend.new"
 
-MODIFYDATE_BACKEND_CONFIG=$(date -r "${CARME_BASE_PATH}"/CarmeConfig.backend "+%d-%m-%Y")
-[[ "${MODIFYDATE_BACKEND_CONFIG}" != "${TODAY}" ]] && die "${CARME_BASE_PATH}/CarmeConfig.backend not modified (last modification ${MODIFYDATE_BACKEND_CONFIG})"
+
+# move carme config backend to right folder
+mv "CarmeConfig.backend.new" "CarmeConfig.backend"
 #-----------------------------------------------------------------------------------------------------------------------------------
