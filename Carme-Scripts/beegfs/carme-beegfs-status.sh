0afd89e0b8279282346e0f65ac7ea7fe337f31a9
Dominik S <dominik.strassel@itwm.fraunhofer.de>
2021-09-10 15:14:48 +0200

    use double brackets in if conditions
    solves issue #255

--- Carme-Scripts/beegfs/carme-beegfs-status.sh
+++ Carme-Scripts/beegfs/carme-beegfs-status.sh
@@ -14,7 +14,7 @@ set -o pipefail
 
 # source basic bash functions ------------------------------------------------------------------------------------------------------
 PATH_TO_SCRIPTS_FOLDER="/opt/Carme/Carme-Scripts"
-if [ -f "${PATH_TO_SCRIPTS_FOLDER}/carme-basic-bash-functions.sh" ];then
+if [[ -f "${PATH_TO_SCRIPTS_FOLDER}/carme-basic-bash-functions.sh" ]];then
   source "${PATH_TO_SCRIPTS_FOLDER}/carme-basic-bash-functions.sh"
 else
   echo "ERROR: carme-basic-bash-functions.sh not found but needed"
@@ -61,7 +61,7 @@ fi
 
 read -rp "Do you want to check the status of BeeGFS? [y/N] " RESP
 echo ""
-if [ "$RESP" = "y" ]; then
+if [[ "$RESP" = "y" ]]; then
 
   # status of the beegfs-mgmtd -----------------------------------------------------------------------------------------------------
   echo "status of the beegfs-mgmtd on $(hostname -s)"
