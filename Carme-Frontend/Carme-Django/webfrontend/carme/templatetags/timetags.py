00c726a8a7b9e0023167b62c87324516ab97616c
carme-admin <carme-admin@carme.ai>
2022-03-07 19:30:39 +0100

    highcharts + better login page

--- Carme-Frontend/Carme-Django/webfrontend/carme/templatetags/timetags.py
+++ Carme-Frontend/Carme-Django/webfrontend/carme/templatetags/timetags.py
@@ -5,9 +5,26 @@ register = template.Library()
 def print_timestamp(timestamp):
     try:
         #assume, that timestamp is given in seconds with decimal point
-        ts = float(timestamp)
+        #ts = float(timestamp)
+        ts = datetime.datetime.fromtimestamp(float(timestamp))
     except ValueError:
         return None
-    return datetime.datetime.fromtimestamp(ts)
+    #return datetime.datetime.fromtimestamp(ts)
+    return ts.strftime('%b %d, %Y, %H:%M')
 
 register.filter(print_timestamp)
+
+def print_timestamp_small(timestamp):
+    try:
+        #assume, that timestamp is given in seconds with decimal point
+        ts = datetime.datetime.fromtimestamp(float(timestamp))
+    except ValueError:
+        return None
+    return ts.strftime('%b %d, %Y')
+
+register.filter(print_timestamp_small)
+
+
+@register.filter
+def index(indexable, i):
+    return indexable[i]
--- Carme-Frontend/Carme-Django/webfrontend/scripts/urls.py
+++ Carme-Frontend/Carme-Django/webfrontend/scripts/urls.py
@@ -1,18 +1,18 @@
-from django.conf.urls import url
+#from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic.base import TemplateView
-from django.urls import include, path
+from django.urls import include, path, re_path
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^', include('carme.urls'), name='home'),
+    re_path(r'^', include('carme.urls'), name='home'),
     #url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    url(r'^logout/$', include('carme.urls'), name='logout'),
+    re_path(r'^logout/$', include('carme.urls'), name='logout'),
     path('carme/', include('carme.urls')),
-    url(r'^notifications/', include('django_nyt.urls')),
-    url(r'^wiki/', include('wiki.urls')),
+    re_path(r'^notifications/', include('django_nyt.urls')),
+    re_path(r'^wiki/', include('wiki.urls')),
     path('todo/', include('todo.urls', namespace="todo")),
     path('admin/', admin.site.urls)
 ]
