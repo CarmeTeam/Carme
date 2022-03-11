37aefd5203b3e9166e451584f063881461c9e726
Dominik S <dominik.strassel@itwm.fraunhofer.de>
2022-03-11 14:27:42 +0100

    check for both conda and mamba init file

--- Carme-Scripts/InsideContainer/start_apps.sh
+++ Carme-Scripts/InsideContainer/start_apps.sh
@@ -50,10 +50,15 @@ echo "export LD_LIBRARY_PATH=\"${LD_LIBRARY_PATH}\"
 
 # activate conda base environment --------------------------------------------------------------------------------------------------
 # NOTE: conda should always be activated not only in interactive shells
-CONDA_INIT_FILE="/opt/miniconda3/etc/profile.d/conda.sh"
-if [[ -f "${CONDA_INIT_FILE}" ]];then
-  log "activate conda base environment"
-  source "${CONDA_INIT_FILE}"
+CONDA_INIT_FILE="/opt/package-manager/etc/profile.d/conda.sh"
+MAMBA_INIT_FILE="/opt/package-manager/etc/profile.d/mamba.sh"
+
+[[ -f "${CONDA_INIT_FILE}" ]] && source "${CONDA_INIT_FILE}"
+[[ -f "${MAMBA_INIT_FILE}" ]] && source "${MAMBA_INIT_FILE}"
+
+if command -v "mamba" >/dev/null 2>&1 ;then
+  mamba activate base
+elif command -v "conda" >/dev/null 2>&1 ;then
   conda activate base
 fi
 #-----------------------------------------------------------------------------------------------------------------------------------
