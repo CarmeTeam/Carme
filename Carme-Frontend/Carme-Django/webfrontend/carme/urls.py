18e58607cf9891960922bff62896281cef83a82a
Gökçehan Kara <goekcehan.kara@itwm.fraunhofer.de>
2024-07-22 11:45:57 +0200

    enable login page in urls.py

--- Carme-Frontend/Carme-Django/webfrontend/carme/urls.py
+++ Carme-Frontend/Carme-Django/webfrontend/carme/urls.py
@@ -26,6 +26,7 @@ except:
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('account/login/', views.myLogin, name='mylogin'),
     path('StartJob/', views.start_job, name='start_job'),
     path('StopJob/', views.stop_job, name='stop_job'),
     path('JobInfo/', views.job_info, name='job_info'),
