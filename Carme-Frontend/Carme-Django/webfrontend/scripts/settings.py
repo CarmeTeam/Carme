8f75200438965efb4ab2691653a473628ed48702
Gökçehan Kara <goekcehan.kara@itwm.fraunhofer.de>
2024-07-22 11:40:50 +0200

    add ldap settings to settings.py

--- Carme-Frontend/Carme-Django/webfrontend/scripts/settings.py
+++ Carme-Frontend/Carme-Django/webfrontend/scripts/settings.py
@@ -13,6 +13,8 @@
 
 import os
 import logging
+import ldap
+from django_auth_ldap.config import LDAPSearch, PosixGroupType, LDAPGroupQuery, GroupOfNamesType
 from django.urls import reverse_lazy
 from importlib.machinery import SourceFileLoader
 SourceFileLoader('CarmeConfig.frontend', '/etc/carme/CarmeConfig.frontend').load_module()
@@ -21,7 +23,7 @@ from CarmeConfig.frontend import *
 # variables ------------------------------------------------------------------------------------------------------------------------
 
 ## security
-DEBUG = False 
+DEBUG = True 
 SECRET_KEY = CARME_KEY
 ALLOWED_HOSTS = ['*', '10.0.0.27']
 
@@ -55,6 +57,8 @@ INSTALLED_APPS = [
     'misaka',
     #'dal',
     #'dal_select2', 
+    'django_otp.plugins.otp_static',
+    'django_otp.plugins.otp_totp',
 ]
 #-----------------------------------------------------------------------------------------------------------------------------------
 
@@ -187,3 +191,128 @@ STATIC_ROOT = '/opt/Carme/Carme-Frontend/Carme-Django/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #-----------------------------------------------------------------------------------------------------------------------------------
+
+# Login ----------------------------------------------------------------------------------------------------------------------------
+LOGIN_URL = 'two_factor:login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = 'account/login/'
+#----------------------------------------------------------------------------------------------------------------------------------
+
+
+# LDAP -----------------------------------------------------------------------------------------------------------------------------
+# NOTE: This section has to be modified according to your specific LDAP setup!
+#       In particualar the variables
+#         - AUTH_LDAP_GROUP_BASE
+#         - AUTH_LDAP_USER_BASE
+#         - AUTH_LDAP_GROUP_FILTER
+#         - AUTH_LDAP_USER_SEARCH or AUTH_LDAP_USER_DN_TEMPLATE
+#         - AUTH_LDAP_GROUP_TYPE
+#         - AUTH_LDAP_REQUIRE_GROUP (if you want to limit access)
+#         - AUTH_LDAP_USER_FLAGS_BY_GROUP
+#         - AUTH_LDAP_USER_ATTR_MAP
+#       have to be checked and (maybe) adjusted.
+#       This has to be done as django is at this point in thime not able to be so generic to work with every possible LDAP setup.
+
+
+# set ldap server uri
+AUTH_LDAP_SERVER_URI = CARME_LDAP_SERVER_PROTO + CARME_LDAP_SERVER_IP
+
+
+# set ldap bind dn
+AUTH_LDAP_BIND_DN = CARME_LDAP_BIND_DN
+
+
+# set ldap password
+AUTH_LDAP_BIND_PASSWORD = CARME_LDAP_SERVER_PW
+
+
+# define ldap base dn
+LDAP_BASE_DN = CARME_LDAP_BASE_DN
+
+
+# set ldap base dn to search for groups
+# NOTE: This is an example!
+AUTH_LDAP_GROUP_BASE = LDAP_BASE_DN
+#AUTH_LDAP_GROUP_BASE = "cn=groups," + LDAP_BASE_DN                                       # example how to modify this variable
+
+
+# set ldap base dn to search for users
+# NOTE: This is an example!
+AUTH_LDAP_USER_BASE = LDAP_BASE_DN
+#AUTH_LDAP_USER_BASE = 'cn=users,' + LDAP_BASE_DN                                         # example how to modify this variable
+
+
+# define ldap group filter
+# NOTE: This is an example!
+AUTH_LDAP_GROUP_FILTER = "(objectClass=PosixGroup)"
+#AUTH_LDAP_GROUP_FILTER = "(&(objectClass=groupOfNames)(|(cn=admins)(cn=users)))"         # example of anohter search filter
+
+
+# format authenticating users distinguished name
+# using ldap user search
+AUTH_LDAP_USER_SEARCH = LDAPSearch(AUTH_LDAP_USER_BASE,
+                                   ldap.SCOPE_SUBTREE, "(uid=%(user)s)")
+# using user dn template
+#AUTH_LDAP_USER_DN_TEMPLATE = 'uid=%(user)s,' + AUTH_LDAP_USER_BASE
+
+
+# format authenticating groups distinguished name using ldap user search
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch(AUTH_LDAP_GROUP_BASE,
+                                    ldap.SCOPE_SUBTREE, AUTH_LDAP_GROUP_FILTER
+                                    )
+
+
+# define ldap group type
+# NOTE: This is an example!
+AUTH_LDAP_GROUP_TYPE = PosixGroupType()
+#AUTH_LDAP_GROUP_TYPE = GroupOfNamesType(name_attr="cn")                                  # example of another group type
+
+
+# restricts groups that are allowed to user CARME
+# NOTE: If you don't want to restrict the access to specific LDAP groups simply comment the AUTH_LDAP_REQUIRE_GROUP variable
+# NOTE: This is an example!
+AUTH_LDAP_REQUIRE_GROUP = (LDAPGroupQuery('cn=admins,' + AUTH_LDAP_GROUP_BASE)
+                           | LDAPGroupQuery('cn=users,' + AUTH_LDAP_GROUP_BASE)
+                          )
+
+
+# set user flags by ldap groups
+# NOTE: If you cannot modify AUTH_LDAP_GROUP_BASE in such a way that it maps all your possible groups modify it directly
+# NOTE: there is no limitation regarding the goups you can add here
+# NOTE: This is an example!
+AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+    'is_active': (
+        LDAPGroupQuery('cn=admins,' + AUTH_LDAP_GROUP_BASE)
+        | LDAPGroupQuery('cn=users,' + AUTH_LDAP_GROUP_BASE)
+    ),
+    'is_staff': 'cn=admins,' + AUTH_LDAP_GROUP_BASE,
+    'is_superuser': 'cn=admins,' + AUTH_LDAP_GROUP_BASE,
+}
+
+
+# mirror a user’s ldap group membership
+AUTH_LDAP_MIRROR_GROUPS = True
+
+
+# populate the django user from the LDAP directory
+# NOTE: This is an example!
+AUTH_LDAP_USER_ATTR_MAP = {
+    "first_name": "cn",
+    "last_name": "cn",
+    "email": "homeDirectory",
+    #"full_name": "displayName",
+}
+
+
+# set authentication backends
+AUTHENTICATION_BACKENDS = [
+    'django_auth_ldap.backend.LDAPBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+
+# enable debug for ldap server connection
+logger = logging.getLogger('django_auth_ldap')
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+# ----------------------------------------------------------------------------------------------------------------------------------
